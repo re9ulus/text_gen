5e92babd18da3b6b8232b542739deb16d894fcf8
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-09-26 23:57:43 +0300

    Update prototyping, add dictionary to distribution logic for markov chain test.

--- src/markov/proto.py
+++ src/markov/proto.py
@@ -1,6 +1,7 @@
 import string
+from collections import Counter
 
-test_string = 'This is sample test string. String is simple. Test string.'
+test_string = 'This is sample test string. String is simple. Test string. Is sample. Another one.'
 
 
 def clear_text(text):
@@ -11,7 +12,7 @@ def text_to_words(text):
 	return text.split()
 
 
-def text_to_dict(words):
+def words_to_dict(words):
 	word_dict = {}
 	for i in range(len(words)-1):
 		word, next_word = words[i], words[i+1]
@@ -20,3 +21,22 @@ def text_to_dict(words):
 		else:
 			word_dict[word] = [next_word]
 	return word_dict
+
+
+def dict_to_distr(word_dict):
+	word_distr = {}
+	for word in word_dict:
+		list_len = len(word_dict[word])
+		word_counts = Counter(word_dict[word])
+		word_distr[word] = {}
+		for target_word in word_counts:
+			word_distr[word][target_word] = word_counts[target_word] / float(list_len)
+	return word_distr
+
+
+if __name__ == '__main__':
+	text = clear_text(test_string)
+	words = text_to_words(text)
+	word_dict = words_to_dict(words)
+	word_distr = dict_to_distr(word_dict)
+	print word_distr
