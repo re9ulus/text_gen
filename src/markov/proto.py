15f0cb5c0b09b4b6ba2990c35e7c0ced515ff963
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-09-27 13:43:51 +0300

    Add bigram to Markov chain

--- src/markov/proto.py
+++ src/markov/proto.py
@@ -16,7 +16,8 @@ class Markov:
     def __init__(self, text):
         self.text = text
         self.words = self._text_to_words(self._clear_text(self.text))
-        self.word_distr = self._dict_to_distr(self._words_to_dict(self.words))
+        self.word_dict = self._words_to_dict_bigram(self.words)
+        self.word_distr = self._dict_to_distr(self.word_dict)
 
     @classmethod
     def _clear_text(cls, text):
@@ -39,14 +40,26 @@ class Markov:
         return word_dict
 
     @staticmethod
+    def _words_to_dict_bigram(words):
+        word_dict = {}
+        for i in range(len(words)-2):
+            word1, word2, next_word = words[i:i+3]
+            key = (word1, word2)
+            if key in word_dict:
+                word_dict[key].append(next_word)
+            else:
+                word_dict[key] = [next_word]
+        return word_dict
+
+    @staticmethod
     def _dict_to_distr(word_dict):
         word_distr = {}
-        for word in word_dict:
-            list_len = len(word_dict[word])
-            word_counts = Counter(word_dict[word])
-            word_distr[word] = {}
+        for key in word_dict:
+            list_len = len(word_dict[key])
+            word_counts = Counter(word_dict[key])
+            word_distr[key] = {}
             for target_word in word_counts:
-                word_distr[word][target_word] = word_counts[target_word] / float(list_len)
+                word_distr[key][target_word] = word_counts[target_word] / float(list_len)
         return word_distr
 
 
@@ -61,6 +74,18 @@ class Markov:
             text += ' ' + next_word
         return text
 
+    def generate_bigram(self, text_length=10):
+        text = ''
+        key = self.word_distr.keys()[np.random.randint(len(self.word_distr))]
+        text = ' '.join(key)
+        for i in range(text_length):
+            if not (key in self.word_distr and len(self.word_distr[key]) > 0):
+                break
+            next_word = np.random.choice(self.word_distr[key].keys(), 1, p=self.word_distr[key].values())[0]
+            text += ' ' + next_word
+            key = (key[-1], next_word)
+        return text
+
 
 if __name__ == '__main__':
     # with codecs.open('./../../test_data/Voina i mir 1.txt', 'r', 'UTF-8', errors='ignore') as f: # './../../test_data/GoT1.txt'
@@ -70,9 +95,10 @@ if __name__ == '__main__':
         test_string = f.read().decode('UTF-8')
 
     mark = Markov(test_string)
-    
+    # print mark.word_distr
+
     with open('res.txt', 'w+') as f:
         for i in range(20):
-            generated = mark.generate()
+            generated = mark.generate_bigram()
             f.write(generated.encode('UTF-8'))
             f.write('\n\n=== === ===\n\n')
