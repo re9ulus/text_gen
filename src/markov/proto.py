170aabf82567143b29e3ac40de0c2283d43c9fbe
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-09-29 22:00:00 +0300

    Update markov proto to work with n-grams

--- src/markov/proto.py
+++ src/markov/proto.py
@@ -16,12 +16,11 @@ class Markov:
     def __init__(self, text):
         self.text = text
         self.words = self._text_to_words(self._clear_text(self.text))
-        self.word_dict = self._words_to_dict_bigram(self.words)
+        self.word_dict = self._words_to_dict_ngram(self.words) #_words_to_dict_bigram(self.words) #
         self.word_distr = self._dict_to_distr(self.word_dict)
 
     @classmethod
     def _clear_text(cls, text):
-        #translate(string.maketrans('', ''), string.punctuation) # don't work with cyliric characters
         return cls.punctuation_regex.sub('', text.lower())
 
     @staticmethod
@@ -30,34 +29,19 @@ class Markov:
 
     @staticmethod
     def _words_to_dict(words):
-        word_dict = {}
+        word_dict = defaultdict(list)
         for i in range(len(words)-1):
             word, next_word = words[i], words[i+1]
-            if word in word_dict:
-                word_dict[word].append(next_word)
-            else:
-                word_dict[word] = [next_word]
+            word_dict[word].append(next_word)
         return word_dict
 
     @staticmethod
-    def _words_to_dict_bigram(words):
-        word_dict = {}
-        for i in range(len(words)-2):
-            word1, word2, next_word = words[i:i+3]
-            key = (word1, word2)
-            if key in word_dict:
-                word_dict[key].append(next_word)
-            else:
-                word_dict[key] = [next_word]
-        return word_dict
-
-    @staticmethod
-    def _words_to_dict_ngram(words, n=2):
+    def _words_to_dict_ngram(words, n=1):
         word_dict = defaultdict(list)
         for i in range(len(words)-n):
             key = tuple(words[i:i+n])
             next_word = words[i+n]
-            word_dict[key] = next_word
+            word_dict[key].append(next_word)
         return word_dict
 
     @staticmethod
@@ -72,39 +56,21 @@ class Markov:
         return word_distr
 
     def generate(self, text_length=10):
-        text = ''
-        next_word = self.word_distr.keys()[np.random.randint(len(self.word_distr))]
-        text = next_word
-        for i in range(text_length):
-            next_word = self.get_next_word(key, self.word_distr)
-            if not next_word:
-                break
-            text += ' ' + next_word
-        return text
+        return self.generate_from_ngram(1, text_length=text_length)
 
     def generate_bigram(self, text_length=10):
-        text = ''
-        key = self.word_distr.keys()[np.random.randint(len(self.word_distr))]
-        text = ' '.join(key)
-        for i in range(text_length - len(key)):
-            next_word = self.get_next_word(key, self.word_distr)
-            if not next_word:
-                break
-            text += ' ' + next_word
-            key = (key[-1], next_word)
-        return text
+        return self.generate_from_ngram(2, text_length=text_length)
 
-    def generate_from_ngram(self, text_length=10):
-        text = ''
+    def generate_from_ngram(self, n, text_length=10):
+        chosen_words = []
         key = self.word_distr.keys()[np.random.randint(len(self.word_distr))]
-        text = ' '.join(key)
-        for i in range(text_length - len(key)):
-            next_word = self.get_next_word(key, self.word_distr)
-            if not next_word:
+        for i in range(text_length - n):
+            word = self.get_next_word(key, self.word_distr)
+            if word is None:
                 break
-            text += ' ' + next_word
-            key = (key[-1], next_word)
-        return text
+            chosen_words.append(word)
+            key = tuple(chosen_words[-n:])
+        return ' '.join(chosen_words)
 
     def get_next_word(self, key, word_distr):
         if not (key in word_distr and len(word_distr[key]) > 0):
@@ -113,17 +79,13 @@ class Markov:
 
 
 if __name__ == '__main__':
-    # with codecs.open('./../../test_data/Voina i mir 1.txt', 'r', 'UTF-8', errors='ignore') as f: # './../../test_data/GoT1.txt'
-    #   test_string = f.read()
-
     with open('./../../test_data/test_text.txt', 'r') as f:
         test_string = f.read().decode('UTF-8')
 
     mark = Markov(test_string)
-    # print mark.word_distr
 
-    with open('./../test_data/res.txt', 'w+') as f:
+    with open('./../../test_data/res.txt', 'w+') as f:
         for i in range(20):
-            generated = mark.generate_bigram(text_length=15)
+            generated = mark.generate(text_length=15)
             f.write(generated.encode('UTF-8'))
             f.write('\n\n=== === ===\n\n')
